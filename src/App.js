f9b170a669c01c610049ec52c2c7a434714fec34
David Andrianarivo <andrianarivo.david@gmail.com>
2023-09-22 16:17:00 +0300

    Setup tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-black">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
