928df5367f36d2d62ef51bcb7214fd859015d6fa
david <andrianarivo.david@gmail.com>
2023-09-27 20:20:40 +0300

    Implement Login design

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { selectAuth } from '../redux/store';
 import { loginUser } from '../redux/auth/authActions';
 import 'react-toastify/dist/ReactToastify.css';
 import useSession from '../hooks/useSession';
 import Input from '../components/Input';
+import Button from '../components/Button';
 
 const Login = () => {
   const [userSignedIn] = useSession();
@@ -40,12 +41,39 @@ const Login = () => {
   }
 
   return (
-    <div className="container mx-auto mt-20">
-      <form className="flex flex-col gap-2" onSubmit={handleSubmit(login)}>
-        <Input register={register} name="email" placeholder="Email" type="email" />
-        <Input register={register} name="password" placeholder="Password" type="password" />
-        <input type="submit" className="border border-green-500" />
-      </form>
+    <div
+      className="bg-home-background-mobile bg-cover bg-center h-screen relative flex justify-center items-center md:bg-home-background"
+    >
+      <div className="w-full max-w-xs">
+        <h1 className="text-white text-center mb-6 text-5xl font-bold">Sign In</h1>
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(login)}>
+          <div className="mb-4">
+            <Input register={register} name="email" placeholder="Email" type="email" />
+          </div>
+          <div className="mb-6">
+            <Input register={register} name="password" placeholder="Password" type="password" />
+          </div>
+          <div className="mb-4 flex justify-center">
+            <a className="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-800" href="/">
+              Forgot Password?
+            </a>
+          </div>
+          <div className="mb-4">
+            <Button className="w-full" text="Sign In" type="submit" />
+          </div>
+          <p className="text-center text-gray-500 text-xs">
+            Don&apos;t have an account ?
+            {' '}
+            <Link to="/sign_up" className="underline">Sign Up</Link>
+          </p>
+          <p className="text-center text-gray-500 text-xs">OR</p>
+          <p className="text-center text-gray-500 text-xs">
+            Go back
+            {' '}
+            <Link to="/" className="underline">Home</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
