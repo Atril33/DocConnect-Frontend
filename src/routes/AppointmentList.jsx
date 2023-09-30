0f96ae3195a755d974db09bab8d8dabbcf60dd3e
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-09-30 01:01:03 -0500

    Implement create appointment and list them

--- src/routes/AppointmentList.jsx
+++ src/routes/AppointmentList.jsx
@@ -16,16 +16,16 @@ const AppointmentList = () => {
   const formatTimeTo24Hour = (timeString) => dayjs(timeString).utc().format('HH:mm:ss');
 
   return (
-    <div className="flex flex-col w-full pt-[50px] justify-between items-center">
-      <h1 className="mb-8 text-2xl font-bold">Your Appointments</h1>
+    <div className="flex flex-col items-center w-11/12 max-w-lg gap-5 mx-auto md:my-10">
+      <h1 className="my-5 text-2xl font-bold md:my-10">Your Appointments</h1>
       {Array.isArray(appointmentsState.appointments) ? (
         <>
           {appointmentsState.appointments.map((appointment) => (
-            <div key={appointment.id} className="flex flex-col w-full md:w-[80%] p-4 md:p-8 mb-4 border border-green-400 rounded-lg shadow-lg">
-              <div className="flex flex-col items-center justify-between doctors-details md:flex-row">
-                <img src={placeholderImage} alt="Doctor's profile" className="w-[100px] md:w-24 rounded-full" />
-                <div className="md:ml-4">
-                  <h2 className="mb-2 text-lg font-semibold md:text-xl">{appointment.doctor.name}</h2>
+            <div key={appointment.id} className="flex flex-col w-full md:w-[80%] p-4 md:p-8 border border-green-400 rounded-lg shadow-lg">
+              <div className="flex items-center justify-around doctors-details">
+                <img src={placeholderImage} alt="Doctor's profile" className="w-[75px]  md:w-24 rounded-full" />
+                <div>
+                  <h2 className="mb-2 text-lg font-semibold text-center md:text-xl">{appointment.doctor.name}</h2>
                   <span className="text-sm text-gray-600">
                     Specialization:
                     {' '}
@@ -33,20 +33,20 @@ const AppointmentList = () => {
                   </span>
                 </div>
               </div>
-              <div className="mt-4 appointment-details md:flex md:justify-between md:items-center">
-                <div className="mb-2 md:mb-0">
+              <div className="mt-4 appointment-details">
+                <div className="mb-2">
                   <span className="text-sm font-semibold">Appointment Date:</span>
                   <span className="ml-2 text-sm">
                     {new Date(appointment.appointment_date).toLocaleDateString()}
                   </span>
                 </div>
-                <div className="mb-2 md:mb-0">
+                <div className="mb-2">
                   <span className="text-sm font-semibold">Appointment Time:</span>
                   <span className="ml-2 text-sm">
                     {formatTimeTo24Hour(appointment.appointment_time)}
                   </span>
                 </div>
-                <div className="mb-2 md:mb-0">
+                <div className="mb-2">
                   <span className="text-sm font-semibold">Appointment Duration:</span>
                   <span className="ml-2 text-sm">
                     {appointment.duration}
@@ -54,7 +54,7 @@ const AppointmentList = () => {
                     minutes
                   </span>
                 </div>
-                <div className="mb-2 md:mb-0">
+                <div className="mb-2">
                   <span className="text-sm font-semibold">Fee:</span>
                   <span className="ml-2 text-sm">
                     {appointment.doctor.fee_per_appointment}
@@ -63,7 +63,7 @@ const AppointmentList = () => {
                   </span>
                 </div>
               </div>
-              <div className="mt-4 buttons md:mt-6">
+              <div className="grid grid-cols-2 gap-2 mt-2 md:mt-4">
                 <button
                   type="button"
       // onClick={() => handleUpdateAppointment(appointment.id)}
@@ -74,7 +74,7 @@ const AppointmentList = () => {
                 <button
                   type="button"
       // onClick={() => handleDeleteAppointment(appointment.id)}
-                  className="w-full px-2 py-1 mt-2 text-white bg-red-500 rounded-md md:w-auto md:mt-0 md:ml-2 hover:bg-red-600"
+                  className="w-full px-2 py-1 text-white bg-red-500 rounded-md md:w-auto md:mt-0 md:ml-2 hover:bg-red-600"
                 >
                   Delete
                 </button>
