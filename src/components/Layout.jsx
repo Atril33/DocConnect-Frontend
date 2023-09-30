b987b3cdbbd9b1a117a3adafe0ea1fe72c6ccbc6
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-09-30 00:12:36 -0500

    Improve mobile responsiveness of side bar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
 const Layout = () => (
-  <div className="lg:flex">
+  <div className="md:flex">
     <Sidebar />
     <Outlet />
   </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   FaTwitter, FaFacebookF, FaGooglePlusG, FaVimeoV, FaPinterestP,
 } from 'react-icons/fa';
@@ -9,13 +9,19 @@ import logo from '../logo.png';
 
 const Sidebar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const directLink = (href) => {
+    toggleMenu();
+    navigate(href);
+  };
+
   return (
     <>
-      <div className="inline-block h-auto p-2 m-2 border-2 border-black rounded-md hover:border-green-400 lg:hidden">
+      <div className="inline-block h-auto p-2 m-2 border-2 border-black rounded-md hover:border-green-400 md:hidden">
         <button
           type="button"
           className="p-2 text-black hover:text-green-400"
@@ -30,52 +36,58 @@ const Sidebar = () => {
             <AiOutlineCloseCircle onClick={toggleMenu} className="text-3xl" />
           </li>
           <li className="px-8 py-4 transition-colors border-b cursor-pointer border-white-700 hover:text-green-400">
-            <Link
-              to="/list-docs"
+            <button
+              type="button"
+              onClick={() => directLink('/list-docs')}
               className="block w-full"
             >
               List of Doctors
-            </Link>
+            </button>
           </li>
           <li className="px-8 py-4 transition-colors border-b cursor-pointer border-white-700 hover:text-green-400">
-            <Link
-              to="/appointment-list"
+            <button
+              type="button"
+              onClick={() => directLink('/appointment-list')}
               className="block w-full"
             >
               Check your Appointments
-            </Link>
+            </button>
           </li>
           <li className="px-8 py-4 transition-colors border-b cursor-pointer border-white-700 hover:text-green-400">
-            <Link
-              to="/create-appointment"
+            <button
+              type="button"
+              onClick={() => directLink('/create-appointment')}
               className="block w-full"
             >
               Schedule a new Appointment
-            </Link>
+            </button>
           </li>
           <li className="px-8 py-4 transition-colors border-b cursor-pointer border-white-700 hover:text-green-400">
-            <Link
-              to="/add-doc"
+            <button
+              type="button"
+              onClick={() => directLink('/add-doc')}
               className="block w-full"
             >
               Add a new Doctor
-            </Link>
+            </button>
           </li>
           <li className="px-8 py-4 transition-colors border-b cursor-pointer border-white-700 hover:text-green-400">
-            <Link
-              to="/delete-doc"
+            <button
+              type="button"
+              onClick={() => directLink('/delete-doc')}
               className="block w-full"
             >
               Delete a Doctor
-            </Link>
+            </button>
           </li>
           <li className="px-8 py-4 transition-colors border-b cursor-pointer border-white-700 hover:text-green-400">
-            <Link
-              to="/logout"
+            <button
+              type="button"
+              onClick={() => directLink('/logout')}
               className="block w-full"
             >
               Sign out
-            </Link>
+            </button>
           </li>
         </ul>
         <ul className="flex flex-row self-end justify-center py-24 align-center">
@@ -86,8 +98,8 @@ const Sidebar = () => {
           <li className="p-[5px]"><FaPinterestP className="transition-colors cursor-pointer hover:text-green-400" /></li>
         </ul>
       </div>
-      <div className="hidden lg:block sticky top-0 w-[25%] h-screen overflow-y-auto no-scrollbar">
-        <div className="text-[#181818] w-full h-screen py-2 border-r-2 border-r-[#f3f3f3] overflow-x-hidden hidden lg:flex flex-col justify-between">
+      <div className="sticky top-0 hidden w-5/12 h-screen max-w-[375px] overflow-y-auto md:block no-scrollbar">
+        <div className="text-[#181818] w-full h-screen py-2 border-r-2 border-r-[#f3f3f3] overflow-x-hidden hidden md:flex flex-col justify-between">
           <Link className="px-4" to="/"><img className="w-[80%] m-auto" src={logo} alt="logo" /></Link>
           <ul className="flex flex-col items-start justify-center gap-2 px-4 text-base sm:text-lg md:text-lg lg:text-xl xl:text-2xl">
             <li className="w-full px-4 py-4 transition-all rounded-xl text-start hover:text-white hover:bg-green-400">
