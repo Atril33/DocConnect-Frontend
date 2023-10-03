c844a85e2e9df0eb972981c5cea199be14d9ced3
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-10-03 14:56:04 +0500

    writing tests for the following routes Login, Logout, Signup

---
+++ src/__test__/routes/Logout.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
+import Logout from '../../routes/Logout';
+
+test('Login component renders correctly', () => {
+  const queryClient = new QueryClient();
+  const component = renderer.create(
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <Logout />
+        </Provider>
+      </QueryClientProvider>
+    </Router>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
